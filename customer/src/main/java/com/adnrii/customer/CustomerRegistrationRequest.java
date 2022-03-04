9cb3f7d60892196bf408c77c76b5d5f3a313ed8f
Mandarin4ek <lukigor61@gmail.com>
2022-03-04 14:08:26 +0200

    Implemented communication between 2 services:(Customer & Fraud).

---
+++ customer/src/main/java/com/adnrii/customer/CustomerRegistrationRequest.java
@@ -0,0 +1,17 @@
+package com.adnrii.customer;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CustomerRegistrationRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+}
---
+++ customer/src/main/java/com/adnrii/customer/CustomerService.java
@@ -0,0 +1,39 @@
+package com.adnrii.customer;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@AllArgsConstructor
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
+
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .build();
+        //todo: check if email valid
+        //todo: check if email not taken
+        /**
+         * Saved in db customer
+         */
+        customerRepository.saveAndFlush(customer);
+        /**
+         * Checks if fraudster
+         */
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+        if (fraudCheckResponse != null && fraudCheckResponse.isFraudster()) {
+            throw new IllegalStateException("fraudster");
+        }
+        //todo: send notification
+    }
+}
